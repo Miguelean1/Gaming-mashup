662f2aa907396a7b89ff43bd9925a3b9180a901e
Mike <mperniaponce2@alumnos.cei.es>
2026-05-07 18:45:43 +0200

    done done

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// server.js
+
 require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
@@ -9,7 +9,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Configurar caché (los datos durarán 1 hora en caché)
+
 const cache = new NodeCache({ stdTTL: 3600 });
 
 app.use(cors());
@@ -19,7 +19,7 @@ app.get('/api/search', async (req, res) => {
     const query = req.query.q;
     if (!query) return res.status(400).json({ error: 'Falta el parámetro de búsqueda (q)' });
 
-    // 1. MECANISMO DE CACHÉ: Comprobar si ya buscamos este juego
+   
     if (cache.has(query.toLowerCase())) {
         console.log(`[CACHE] Devolviendo resultados cacheados para: ${query}`);
         return res.json({ source: 'cache', data: cache.get(query.toLowerCase()) });
@@ -28,7 +28,7 @@ app.get('/api/search', async (req, res) => {
     try {
         console.log(`[API] Haciendo peticiones reales para: ${query}`);
         
-        // 2. PETICIONES A LAS 3 APIs EN PARALELO
+        
         const rawgUrl = `https://api.rawg.io/api/games?search=${encodeURIComponent(query)}&key=${process.env.RAWG_API_KEY}&page_size=1`;
         const cheapSharkUrl = `https://www.cheapshark.com/api/1.0/games?title=${encodeURIComponent(query)}&limit=3`;
         const redditUrl = `https://www.reddit.com/search.json?q=${encodeURIComponent(query)}+subreddit:gaming&limit=3`;
@@ -39,17 +39,17 @@ app.get('/api/search', async (req, res) => {
             axios.get(redditUrl)
         ]);
 
-        // Construir el objeto de respuesta combinado (Mashup)
+      
         const resultData = {
             gameInfo: rawgRes.data.results[0] || null,
             deals: cheapRes.data || [],
             discussions: redditRes.data.data.children.map(child => child.data) || []
         };
 
-        // Guardar en la caché para futuras peticiones idénticas
+       
         cache.set(query.toLowerCase(), resultData);
 
-        // Enviar al frontend
+      
         res.json({ source: 'api', data: resultData });
 
     } catch (error) {
